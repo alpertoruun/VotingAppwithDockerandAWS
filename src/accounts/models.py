08f9c5c1a722b19357f9476b07750d03f73ca4ef
Alper Torun <alpertorun4@hotmail.com>
2024-04-25 22:02:09 +0300

    first commit

---
+++ src/accounts/models.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+
+from flask_login import UserMixin
+
+from src import bcrypt, db
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, is_admin=False):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.created_on = datetime.now()
+        self.is_admin = is_admin
+
+    def __repr__(self):
+        return f"<email {self.email}>"
+
+class Voter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tc = db.Column(db.String(11), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    face_recognitions = db.relationship('FaceRecognition', backref='voter', lazy=True)
+
+class Election(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    start_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    end_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    options = db.relationship('Option', backref='election', lazy=True)
+    votes = db.relationship('Votes', backref='election', lazy=True)
+
+class Option(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    election_id = db.Column(db.Integer, db.ForeignKey('election.id'), nullable=False)
+    votes = db.relationship('Votes', backref='option', lazy=True)
+
+class Votes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voter_id = db.Column(db.Integer, db.ForeignKey('voter.id'), nullable=False)
+    election_id = db.Column(db.Integer, db.ForeignKey('election.id'), nullable=False)
+    option_id = db.Column(db.Integer, db.ForeignKey('option.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class OptionCount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    election_id = db.Column(db.Integer, db.ForeignKey('election.id'), nullable=False)
+    option_id = db.Column(db.Integer, db.ForeignKey('option.id'), nullable=False)
+    vote_count = db.Column(db.Integer, default=0)
+
+class FaceRecognition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voter_id = db.Column(db.Integer, db.ForeignKey('voter.id'), nullable=False)
+    encoding = db.Column(db.LargeBinary, nullable=False)  
+
+class VoteToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(256), unique=True, nullable=False)
+    voter_id = db.Column(db.Integer, db.ForeignKey('voter.id'), nullable=False)
+    election_id = db.Column(db.Integer, db.ForeignKey('election.id'), nullable=False)
+    used = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    voter = db.relationship('Voter', backref=db.backref('tokens', lazy=True))
+    election = db.relationship('Election', backref=db.backref('tokens', lazy=True))
+
+class PasswordResetToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    token = db.Column(db.String(256), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    used = db.Column(db.Boolean, default=False, nullable=False)
+    user = db.relationship('User', backref=db.backref('password_reset_tokens', lazy=True))
