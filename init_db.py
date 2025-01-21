03475862ab601a8e5f6d09b0c98f44386b2b4749
Alper Torun <alper.torun@piworks.net>
2025-01-21 12:24:55 +0300

    fix: removes unnecessary files

--- init_db.py
+++
@@ -1,7 +0,0 @@
-from src import db, app
-from src.accounts.models import *  # Tüm modelleri içe aktar
-
-with app.app_context():
-    print("Veritabanı tabloları oluşturuluyor...")
-    db.create_all()
-    print("Veritabanı tabloları başarıyla oluşturuldu.")
